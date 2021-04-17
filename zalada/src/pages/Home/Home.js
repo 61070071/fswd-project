3a9987876523ef5908172e3a6865bd5f3207bd53
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-17 16:10:08 +0700

    Update Home.js

--- zalada/src/pages/Home/Home.js
+++ zalada/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.css";
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import { Carousel, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import C1img from "../../images/carousel/crs_1.png";
 import C2img from "../../images/carousel/crs_2.png";
@@ -14,33 +14,37 @@ function Home() {
         <Carousel.Item interval={1000}>
           <img className="d-block w-100" src={C1img} alt="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>DISCOUNT UP TO 30%</h3>
+            <p>MORE AND MORE PROMOTION</p>
+            <Button variant="outline-light">DISCOVER NOW</Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img className="d-block w-100" src={C2img} alt="Second slide" />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>LOOKING FOR ELECTRIC STUFF?</h3>
+            <p>SAY NO MORE, FAM</p>
+            <Button variant="outline-light">DISCOVER NOW</Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img className="d-block w-100" src={C3img} alt="Third slide" />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>STOCKING UP THESE FOOD</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
+            <Button variant="outline-light">DISCOVER NOW</Button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={C4img} alt="Fourth slide" />
           <Carousel.Caption>
-            <h3>Fourth slide label</h3>
+            <h3>PACKING YOUR STUFF</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
+            <Button variant="outline-light">DISCOVER NOW</Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
