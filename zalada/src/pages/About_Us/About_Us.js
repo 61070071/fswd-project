565525c4fcc4983e40c7535d98d9a2f15115d28c
forsakemenot <thanapon.asd@gmail.com>
2021-04-17 16:53:41 +0700

    sub carousel

--- zalada/src/pages/About_Us/About_Us.js
+++ zalada/src/pages/About_Us/About_Us.js
@@ -1,5 +1,87 @@
-function About_Us() {
-    return <h2>About us</h2>;
-  }
+import "./About_Us.css";
 
-  export default About_Us;
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+function About_Us() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center flex-column">
+        <div id="left_area" className="d-flex justify-content-center flex-50">
+          <h1>TEST</h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="">
+            <Slider {...settings}>
+              <div className="bg-primary">
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+              <div>
+                <h3>7</h3>
+              </div>
+              <div>
+                <h3>8</h3>
+              </div>
+              <div>
+                <h3>9</h3>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About_Us;
