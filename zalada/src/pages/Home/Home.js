6a5d03574b78d1f7d442e497ef2b79f59fd5b45e
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-17 15:54:21 +0700

    Update Home.js

--- zalada/src/pages/Home/Home.js
+++ zalada/src/pages/Home/Home.js
@@ -2,41 +2,42 @@ import "./Home.css";
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
+import C1img from "../../images/carousel/crs_1.png";
+import C2img from "../../images/carousel/crs_2.png";
+import C3img from "../../images/carousel/crs_3.png";
+import C4img from "../../images/carousel/crs_4.png";
 function Home() {
   return (
     <div className="container test">
-      <h2 >Home</h2>
+      <h2>Home</h2>
       <Carousel>
         <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={C1img} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src={C2img} alt="Second slide" />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
+        <Carousel.Item interval={500}>
+          <img className="d-block w-100" src={C3img} alt="Third slide" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={C4img} alt="Fourth slide" />
+          <Carousel.Caption>
+            <h3>Fourth slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
