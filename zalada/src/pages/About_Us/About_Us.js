5272753409673e9fe7f05143834bdf001159e9d6
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-17 18:41:27 +0700

    Update Sub Carousel

--- zalada/src/pages/About_Us/About_Us.js
+++ zalada/src/pages/About_Us/About_Us.js
@@ -1,12 +1,15 @@
 import "./About_Us.css";
 
-import Carousel from "react-bootstrap/Carousel";
+import { Card, Button, Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React, { Component } from "react";
 import Slider from "react-slick";
-
+import C1img from "../../images/carousel/crs_1.png";
+import C2img from "../../images/carousel/crs_2.png";
+import C3img from "../../images/carousel/crs_3.png";
+import C4img from "../../images/carousel/crs_4.png";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -29,55 +32,66 @@ function SamplePrevArrow(props) {
     />
   );
 }
+
+function Card_Carousal(props) {
+  return (
+    <Card style={{ width: "16rem" ,marginLeft:"2%",marginRight:"2%"}}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Button variant="primary">Go somewhere</Button>
+      <Card.Body>
+        <Card.Title>{props.product}</Card.Title>
+        <Card.Text>
+          1553 ฿
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+  );
+}
 function About_Us() {
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
-    <div className="container">
-      <div className="d-flex justify-content-center flex-column">
-        <div id="left_area" className="d-flex justify-content-center flex-50">
+    <div className="">
+      <div className="d-flex flex-row  justify-content-around f-con">
+        <div id="left_area" className="left-area">
           <h1>TEST</h1>
         </div>
-        <div className="d-flex justify-content-center">
-          <div className="">
-            <Slider {...settings}>
-              <div className="bg-primary">
-                <h3>1</h3>
-              </div>
-              <div>
-                <h3>2</h3>
-              </div>
-              <div>
-                <h3>3</h3>
-              </div>
-              <div>
-                <h3>4</h3>
-              </div>
-              <div>
-                <h3>5</h3>
-              </div>
-              <div>
-                <h3>6</h3>
-              </div>
-              <div>
-                <h3>7</h3>
-              </div>
-              <div>
-                <h3>8</h3>
-              </div>
-              <div>
-                <h3>9</h3>
-              </div>
-            </Slider>
-          </div>
+
+        <div className="d-flex sub-slide align-items-center">
+          {/* <Slider {...settings} className="bg-secondary ">
+            <Card_Carousal />
+            <Card_Carousal />
+            <Card_Carousal />
+            <Card_Carousal />
+            <Card_Carousal />
+            <Card_Carousal />
+          </Slider> */}
+          <Carousel fade id="crs" className="">
+            <Carousel.Item className="d-flex flex-row justify-content-center">
+              <Card_Carousal product="Jacket 1"/>
+              <Card_Carousal product="Jacket 2"/>
+              <Card_Carousal product="Jacket 3"/>
+            </Carousel.Item>
+            <Carousel.Item className="d-flex flex-row justify-content-center">
+              <Card_Carousal product="Jacket 4"/>
+              <Card_Carousal product="Jacket 5"/>
+              <Card_Carousal product="Jacket 6"/>
+            </Carousel.Item>
+            <Carousel.Item  className="d-flex flex-row justify-content-center">
+              <Card_Carousal product="Jacket 7"/>
+              <Card_Carousal product="Jacket 8"/>
+              <Card_Carousal product="Jacket 9"/>
+            </Carousel.Item>
+          </Carousel>
         </div>
       </div>
     </div>
