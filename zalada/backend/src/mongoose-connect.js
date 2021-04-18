cf02560709b8752276f7021bdfb5287ff544ab75
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-19 00:57:20 +0700

    Add Backend

---
+++ zalada/backend/src/mongoose-connect.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+mongoose.Promise = Promise
+mongoose.connect(
+  'mongodb+srv://cluster0.qrvc8.mongodb.net',
+  {
+    dbName: 'test',
+    user: DB_USERNAME,
+    pass: DB_PASSWORD,
+    promiseLibrary: Promise,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+)
