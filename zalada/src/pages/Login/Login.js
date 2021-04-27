d7cbecd1a371f1636bc8da64b4e3ba509f5dfc5f
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-27 22:15:57 +0700

    Update Login

--- zalada/src/pages/Login/Login.js
+++ zalada/src/pages/Login/Login.js
@@ -1,27 +1,61 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Login.css";
 import HomeLogo from "../../images/Zalada.svg"
-import { Link } from "react-router-dom";
+import React, { useCallback, useState } from 'react'
+import { useHistory, Link } from 'react-router-dom'
 
-
+import { useSession } from '../../contexts/SessionContext'
 function Login() {
+    const history = useHistory()
+    const { login } = useSession()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const handleUsernameChange = useCallback(
+        (e) => {
+            setUsername(e.target.value)
+        },
+        [],
+    )
+    const handlePasswordChange = useCallback(
+        (e) => {
+            setPassword(e.target.value)
+        },
+        [],
+    )
+    const handleLogin = useCallback(
+        async (e) => {
+            e.preventDefault()
+            await login(username, password)
+            // alert("success")
+            history.push('/')
+            window.location.reload(); 
+        },
+        [login, password, username, history],
+    )
+    // const redirectToRegister = useCallback(
+    //     () => {
+    //         history.push('/register')
+    //     },
+    //     [history],
+    // )
     return (
         <div className="login d-flex justify-content-center mt-5">
             <div className="login_face d-flex flex-column align-items-center">
                 <div className="mt-5">
                     <Link to="/"><img alt="" className="home_logo" src={HomeLogo} /></Link>
                 </div>
-                <div className="mt-5">
-                    <div className="">
-                        <input className="" type="text" placeholder="USERNAME" />
-                    </div>
+                <form onSubmit={handleLogin}>
                     <div className="mt-5">
-                        <input className="" type="text" placeholder="PASSWORD" />
+                        <div>
+                            <input type="text" placeholder="USERNAME" value={username} onChange={handleUsernameChange} required />
+                        </div>
+                        <div className="mt-5">
+                            <input type="password" placeholder="PASSWORD" value={password} onChange={handlePasswordChange} required />
+                        </div>
+                        <Link to="#" className="d-flex justify-content-end likable forgot">FORGOT PASSWORD ?</Link>
                     </div>
-                    <Link to="#" className="d-flex justify-content-end likable forgot">FORGOT PASSWORD ?</Link>
-                </div>
-                <button type="button" class="btn btn-primary btn-lg mt-5 login_btn" >LOGIN</button>
+                    <button type="submit" class="btn btn-primary btn-lg mt-5 login_btn" >LOGIN</button>
+                </form>
                 <span className="mt-1">or you haven't an account yet ? <Link to="#" className="likable">Register</Link></span>
             </div>
         </div>
