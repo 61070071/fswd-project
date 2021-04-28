767a752c3db10af673b06d12f96239979440bb73
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-04-29 00:05:54 +0700

    Update PromotionProduct.js

--- zalada/src/components/PromotionProduct/PromotionProduct.js
+++ zalada/src/components/PromotionProduct/PromotionProduct.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function PromotionProduct({ data }) {
     return (
         <Link to="/details"><div className="d-flex flex-wrap justify-content-center box_product">
-            <img alt="" src={data.photourl} />
+            <img alt="" src={data.photourl[0]} />
             <span className="d-flex mx-auto justify-content-center box_pro">{data.description}</span>
             <span className="d-flex flex-100 justify-content-center text_name">{data.productname}</span>
             <span className="d-flex flex-100 justify-content-center text_price color-main">{data.price}</span>
