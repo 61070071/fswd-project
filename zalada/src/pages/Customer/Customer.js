91226d94fbee1bcf4f3a72efea1ab577bcdef88b
Faiifu <42910426+Faiifu@users.noreply.github.com>
2021-04-29 21:14:49 +0700

    add customer page

---
+++ zalada/src/pages/Customer/Customer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Customer.css"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Customer() {
+  return (
+    <div>
+        <p>dik</p>
+    </div>
+  );
+}
+export default Customer;
