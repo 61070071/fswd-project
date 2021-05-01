64eff3df993a050a3e91d49e85bce5c72a5ef312
Faiifu <42910426+Faiifu@users.noreply.github.com>
2021-05-01 23:48:37 +0700

    update admin promotion search bar

--- zalada/src/pages/Admin/AdminPromotions/AdminPromotions.js
+++ zalada/src/pages/Admin/AdminPromotions/AdminPromotions.js
@@ -6,43 +6,44 @@ import Plus from "../../../images/circle-plus.svg"
 import AddAdminPromotions from "../../../components/AddAdminPromotions/AddAdminPromotions"
 import AddAdminPromotionOnsale from "../../../components/AddAdminPromotionOnsale/AddAdminPromotionOnsale"
 import Shirt from "../../../images/brown_shirt.svg"
+import SearchLogo from "../../../images/search.svg"
 
 function AdminPromotions() {
     const products = [{
-        id:"001",
-        name:"LEATHER JACKET",
-        image:Shirt,
-        stock:12,
-        condition:"BUY 2",
-        get:"1",
+        id: "001",
+        name: "LEATHER JACKET",
+        image: Shirt,
+        stock: 12,
+        condition: "BUY 2",
+        get: "1",
         totalDeals: "30%",
         totalSave: "30% OFF",
-        promotionStart:"12 / 5 / 2564",
-        promotionEnd:"13 / 5 / 2564"
+        promotionStart: "12 / 5 / 2564",
+        promotionEnd: "13 / 5 / 2564"
     },
     {
-        id:"002",
-        name:"LEATHER JACKET",
-        image:Shirt,
-        stock:13,
-        condition:"BUY 2",
-        get:"1",
+        id: "002",
+        name: "LEATHER JACKET",
+        image: Shirt,
+        stock: 13,
+        condition: "BUY 2",
+        get: "1",
         totalDeals: "30%",
         totalSave: "30% OFF",
-        promotionStart:"10 / 5 / 2564",
-        promotionEnd:"11 / 5 / 2564"
+        promotionStart: "10 / 5 / 2564",
+        promotionEnd: "11 / 5 / 2564"
     },
     {
-        id:"003",
-        name:"LEATHER JACKET",
-        image:Shirt,
-        stock:14,
-        condition:"BUY 2",
-        get:"1",
+        id: "003",
+        name: "LEATHER JACKET",
+        image: Shirt,
+        stock: 14,
+        condition: "BUY 2",
+        get: "1",
         totalDeals: "30%",
         totalSave: "30% OFF",
-        promotionStart:"09 / 5 / 2564",
-        promotionEnd:"14 / 6 / 2564"
+        promotionStart: "09 / 5 / 2564",
+        promotionEnd: "14 / 6 / 2564"
     }]
     return (
         <div className="d-flex flex-wrap">
@@ -54,27 +55,33 @@ function AdminPromotions() {
                 <div className="w-100 mx-auto h-100 bg-main">
                     <div className="w-90 mx-auto bg-white ">
                         <div className="w-100 d-flex justify-content-end py-1-v bg-main">
-                            <button className="btn-add-product-admin color-white font-weight-bold d-flex align-items-center">ADD PRODUCT <img alt="" src={Plus} /></button>
+                            <button className="btn-add-product-admin color-white font-weight-bold d-flex align-items-center">ADD PROMOTION <img alt="" src={Plus} /></button>
                         </div>
                         <div className="w-100 box-add-product">
-                            <div className="w-100 d-flex top-list-add-product">
-                                <div className="w-55">
+                            <div className="w-100 d-flex bar_select d-flex align-items-end">
+                                <div className="w-30">
                                     <span className="color-second">PRODUCT</span>
                                 </div>
-                                <div className="w-45 d-flex">
-                                    <div className="w-50">
-                                        <span className="color-second">PRODUCT TYPE : </span>
-                                        <select className="w-50 select-product">
+                                <div className="w-40">
+                                    <div className="d-flex align-items-center justify-content-end mr-2-v">
+                                        <img alt="" className="search_input position-absolute" src={SearchLogo} />
+                                        <input className="input-nav-bar" type="text" placeholder="Search and hit enter..." />
+                                    </div>
+                                </div>
+                                <div className="w-50 d-flex">
+                                    <div className="d-flex align-items-center">
+                                        <span className="fs-0-8-v">PRODUCT TYPE : </span>
+                                        <select className="select_display mb-0 fs-0-8-v">
                                             <option value="ALL">ALL</option>
                                             <option value="A">A</option>
                                             <option value="B">B</option>
                                             <option value="C">C</option>
                                         </select>
                                     </div>
-                                    <div className="w-50 d-flex justify-content-center align-items-center">
-                                        <span className="color-second">SORT BY : </span>
-                                        <select className="w-50 select-product">
-                                            <option value="ALL">RENCENTLY</option>
+                                    <div className="d-flex align-items-center ml-2-v">
+                                        <span className="fs-0-8-v">SORT BY : </span>
+                                        <select className="select_display mb-0 fs-0-8-v">
+                                            <option value="ALL">RECENTLY ADD</option>
                                             <option value="A">A</option>
                                             <option value="B">B</option>
                                             <option value="C">C</option>
@@ -85,22 +92,18 @@ function AdminPromotions() {
                             <div className="w-100">
                                 {
                                     products.map(function (element, index) {
-                                        return <AddAdminPromotions data={element}/>
+                                        return <AddAdminPromotions data={element} />
                                     })
                                 }
                                 {
                                     products.map(function (element, index) {
-                                        return <AddAdminPromotionOnsale data={element}/>
+                                        return <AddAdminPromotionOnsale data={element} />
                                     })
                                 }
-                                
                             </div>
                         </div>
                     </div>
-
-
                 </div>
-
             </div>
 
         </div>
