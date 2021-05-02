35e917e8c7f02adc7c0dbc70248a0caa0067313f
Faiifu <42910426+Faiifu@users.noreply.github.com>
2021-05-02 20:57:53 +0700

    update checkout

--- zalada/src/pages/Account_Info/Account_Info.js
+++ zalada/src/pages/Account_Info/Account_Info.js
@@ -6,64 +6,56 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import CartLogo from "../../images/shopping-cart-black-shape.svg"
 
 function Account_Info() {
-  return (
-      <div className="container">
-            <div className="row d-flex justify-content-center">
-                <div className="col-8 mt-5 p-0">
-                    <div className="d-flex align-items-center">
-                        <p className="cart_header">ACCOUNT INFORMATION</p>
-                    </div>
-
-                    <div className="form_info mt-2 mb-5 d-flex flex-column justify-content-center align-items-center">
-                        <div className="w-100">
-                            <div>
-                                <label className="form-label">Firstname</label>
-                                <input className="form-control mb-3" type="text" placeholder="Firstname"></input>
-                            </div>
-                            <div>
-                                <label className="form-label">Phone Number</label>
-                                <input className="form-control mb-3" type="number" placeholder="+66"></input>
-                            </div>
-                            <div>
-                                <label className="form-label">Address</label>
-                                <textarea className="form-control mb-3" type="text" placeholder="Address..." rows="4"></textarea>
-                            </div>
-
-                            <div className="address d-flex justify-content-between mb-3">
-                                <div>
-                                    <p>Province</p>
-                                    <select className="dropdown_address">
-                                        <option></option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <p>District</p>
-                                    <select className="dropdown_address">
-                                        <option></option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <p>Sub-District</p>
-                                    <select className="dropdown_address">
-                                        <option></option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div>
-                                <label className="form-label">Post number</label>
-                                <input className="form-control mb-5 w-25" type="number" placeholder="10510" rows="4"></input>
-                            </div>
-
-                            <div>
-                                <button className="btn btn_accountInfo w-100">CONTINUE</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+   return (
+      <div className="bg-cloud">
+         <div className="w-50 mx-auto">
+            <p className="cart_header">ACCOUNT INFORMATION</p>
+            <div className="form_info">
+               <div className="">
+                  <div>
+                     <label className="form-label fs-1-v">Firstname</label>
+                     <input className="form-control mb-2-v" type="text" placeholder="Firstname"></input>
+                  </div>
+                  <div>
+                     <label className="form-label fs-1-v">Phone Number</label>
+                     <input className="form-control mb-2-v" type="number" placeholder="+66"></input>
+                  </div>
+                  <div>
+                     <label className="form-label fs-1-v">Address</label>
+                     <textarea className="form-control mb-2-v" type="text" placeholder="Address..." rows="4"></textarea>
+                  </div>
+                  <div className="address d-flex justify-content-between mb-2-v">
+                     <div>
+                        <p className="fs-1-v m-0">Province</p>
+                        <select className="dropdown_address">
+                           <option></option>
+                        </select>
+                     </div>
+                     <div>
+                        <p className="fs-1-v m-0">District</p>
+                        <select className="dropdown_address">
+                           <option></option>
+                        </select>
+                     </div>
+                     <div>
+                        <p className="fs-1-v m-0">Sub-District</p>
+                        <select className="dropdown_address">
+                           <option></option>
+                        </select>
+                     </div>
+                  </div>
+                  <div>
+                     <label className="form-label fs-1-v">Post number</label>
+                     <input className="form-control mb-2-v w-25" type="number" placeholder="10510" rows="4"></input>
+                  </div>
+                  <div>
+                     <button className="btn btn_accountInfo w-100">CONTINUE</button>
+                  </div>
+               </div>
             </div>
+         </div>
       </div>
-    );
+   );
 }
 
 export default Account_Info;
