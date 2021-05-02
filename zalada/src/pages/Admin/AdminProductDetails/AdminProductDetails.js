3fbf54a9530fbeaa129f93a11e2ac6394fc56232
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-05-03 02:22:16 +0700

    Update Product

--- zalada/src/pages/Admin/AdminProductDetails/AdminProductDetails.js
+++ zalada/src/pages/Admin/AdminProductDetails/AdminProductDetails.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { useCallback, useState } from 'react'
 import { useHistory } from 'react-router'
@@ -7,7 +7,7 @@ import AdminNavBar from "../../../components/AdminNavBar/AdminNavBar";
 import UploadPicture from "../../../images/upload-picture.svg";
 import Photo from "../../../images/add-a-photo.svg";
 import { PRODUCTS_UPDATE } from '../../../graphql/productsOGQuery';
-import {isNumeric} from '../../../util/isNumeric'
+import { isNumeric } from '../../../util/isNumeric'
 import { useLazyQuery } from '@apollo/client'
 import { PRODUCT_QUERY_ID } from '../../../graphql/productsOGQuery';
 
@@ -22,7 +22,17 @@ function AdminProductDetails() {
         variables: { productId },
         onCompleted: data => {
             // console.log(data);
-            setProduct(data?.productById)
+            // setProduct(data?.productById)
+            setProduct({
+                catagory: data?.productById.catagory,
+                isActive: data?.productById.isActive,
+                photourl: data?.productById.photourl,
+                price: data?.productById.price,
+                productdescription: data?.productById.productdescription,
+                productname: data?.productById.productname,
+                quantity: data?.productById.quantity
+            })
+
         }
     })
     const data = product
@@ -39,9 +49,9 @@ function AdminProductDetails() {
     const [createProduct] = useMutation(PRODUCTS_UPDATE)
     const handleInputChange = useCallback(
         (e) => {
-            
+
             let { name, value } = e.target
-            if(isNumeric(value)) value = Number(value)
+            if (isNumeric(value)) value = Number(value)
             setProduct((prev) => ({ ...prev, [name]: value }))
         },
         [],
@@ -50,14 +60,14 @@ function AdminProductDetails() {
         async (e) => {
             e.preventDefault()
             try {
-                await createProduct({ variables:  productId , record: product })
+                await createProduct({ variables: { record: product, productId } })
                 history.push('/')
                 alert('Product Updated')
             } catch (err) {
                 console.log(err)
                 alert('Product Update failed')
                 console.log(product);
-                
+
             }
         },
         [createProduct, history, product],
@@ -67,7 +77,7 @@ function AdminProductDetails() {
         <div className="d-flex flex-wrap">
 
             <div className="w-15">
-                <AdminNavBar page={2}/>
+                <AdminNavBar page={2} />
             </div>
             <div className="w-85 d-flex flex-wrap align-items-start">
                 <div className="w-100 h-90 bg-main d-flex py-1-5-v">
@@ -75,7 +85,7 @@ function AdminProductDetails() {
                         <div className="w-95 mx-auto line-bottom-gray">
                             <div className="w-30 py-0-5-v mt-1-v d-flex align-items-center">
                                 <span className="color-second mr-1-v">UPDATE PRODUCTS</span>
-                                
+
                             </div>
                         </div>
                         {/* form อยู่นี้ */}
@@ -89,12 +99,12 @@ function AdminProductDetails() {
                                     <div className="w-100 d-flex flex-column mt-0-5-v color-no-5">
                                         <span className="py-0-5-v">QUANTITY</span>
                                         <input onChange={handleInputChange} value={product.quantity} type="number" name="quantity" className="w-70 bg-third rounded bd-1-gray" />
-                                        
-                                        
+
+
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-0-5-v color-no-5">
                                         <span className="py-0-5-v">PRICE</span>
-                                        <input onChange={handleInputChange} value={product.price} type="number"  name="price" className="w-70 bg-third rounded bd-1-gray" />
+                                        <input onChange={handleInputChange} value={product.price} type="number" name="price" className="w-70 bg-third rounded bd-1-gray" />
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-1-v color-no-5">
                                         <span className="py-0-5-v">CATEGORY</span>
@@ -109,7 +119,7 @@ function AdminProductDetails() {
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-1-v color-no-5">
                                         <span className="py-0-5-v">DESCRIPTION</span>
-                                        <textarea onChange={handleInputChange} value={product.productdescription}  name="productdescription" className="w-70 bg-third rounded bd-1-gray" rows="4" cols="50"></textarea>
+                                        <textarea onChange={handleInputChange} value={product.productdescription} name="productdescription" className="w-70 bg-third rounded bd-1-gray" rows="4" cols="50"></textarea>
                                     </div>
                                     <button className="w-70 mt-2-v bg-second color-white rounded border-0 py-0-5-v font-weight-bold" type="submit">UPDATE</button>
                                 </div>
@@ -122,7 +132,7 @@ function AdminProductDetails() {
                                             <span >UPLOAD PICTURE</span>
                                             <img alt="" src={Photo} />
                                             {/* <input onChange={handleInputChange} value={product.photourl} name="photourl" type="file" className="position-absolute w-100 h-100 op-0"/> */}
-                                            <input onChange={handleInputChange} value={product.photourl}  name="photourl" className="w-70 bg-third rounded bd-1-gray" />
+                                            <input onChange={handleInputChange} value={product.photourl} name="photourl" className="w-70 bg-third rounded bd-1-gray" />
                                         </div>
                                     </div>
 
