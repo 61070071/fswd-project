de445663442103a7f1b34a0646deed9f969e12b0
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2021-05-03 07:05:05 +0700

    Update

--- zalada/src/pages/Admin/AdminUpdateOnsale/AdminUpdateOnsale.js
+++ zalada/src/pages/Admin/AdminUpdateOnsale/AdminUpdateOnsale.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { useCallback, useState } from 'react'
 import { useHistory } from 'react-router'
@@ -7,7 +7,7 @@ import { useMutation } from '@apollo/client'
 import AdminNavBar from "../../../components/AdminNavBar/AdminNavBar";
 import plus from "../../../images/add-circle.svg";
 import { PRODUCTS_UPDATE } from '../../../graphql/productsOGQuery';
-import {isNumeric} from '../../../util/isNumeric'
+import { isNumeric } from '../../../util/isNumeric'
 import { useLazyQuery } from '@apollo/client'
 import { PRODUCT_QUERY_ID } from '../../../graphql/productsOGQuery';
 
@@ -39,9 +39,9 @@ function AdminUpdateOnsale() {
     const [createProduct] = useMutation(PRODUCTS_UPDATE)
     const handleInputChange = useCallback(
         (e) => {
-            
+
             let { name, value } = e.target
-            if(isNumeric(value)) value = Number(value)
+            if (isNumeric(value)) value = Number(value)
             setProduct((prev) => ({ ...prev, [name]: value }))
         },
         [],
@@ -50,14 +50,14 @@ function AdminUpdateOnsale() {
         async (e) => {
             e.preventDefault()
             try {
-                await createProduct({ variables:  productId , record: product })
+                await createProduct({ variables: productId, record: product })
                 history.push('/')
                 alert('Product Updated')
             } catch (err) {
                 console.log(err)
                 alert('Product Update failed')
                 console.log(product);
-                
+
             }
         },
         [createProduct, history, product],
@@ -67,7 +67,7 @@ function AdminUpdateOnsale() {
         <div className="d-flex flex-wrap">
 
             <div className="w-15">
-                <AdminNavBar page={2}/>
+                <AdminNavBar page={2} />
             </div>
             <div className="w-85 d-flex flex-wrap align-items-start">
                 <div className="w-100 h-90 bg-main d-flex py-1-5-v">
@@ -75,7 +75,7 @@ function AdminUpdateOnsale() {
                         <div className="w-95 mx-auto line-bottom-gray">
                             <div className="w-30 py-0-5-v mt-1-v d-flex align-items-center">
                                 <span className="color-second mr-1-v">UPDATE PRODUCTS</span>
-                                <img alt="" src={plus}/>
+                                <img alt="" src={plus} />
                             </div>
                         </div>
 
@@ -93,19 +93,19 @@ function AdminUpdateOnsale() {
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-0-5-v color-no-5">
                                         <span className="py-0-5-v">QUANTITY</span>
-                                        <input onChange={handleInputChange} value={product.quantity} type="number"  name="quantity" className="w-70 bg-third rounded bd-1-gray" />
+                                        <input onChange={handleInputChange} value={product.quantity} type="number" name="quantity" className="w-70 bg-third rounded bd-1-gray" />
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-1-v color-no-5">
                                         <span className="py-0-5-v">PRICE</span>
-                                        <input onChange={handleInputChange} value={product.price} type="number"  name="price" className="w-70 bg-third rounded bd-1-gray" />
+                                        <input onChange={handleInputChange} value={product.price} type="number" name="price" className="w-70 bg-third rounded bd-1-gray" />
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-1-v color-no-5">
                                         <span className="py-0-5-v">DISCOUNT (%)</span>
-                                        <input onChange={handleInputChange} value={product.discount} type="number"  name="discount" className="w-35 bg-third rounded bd-1-gray" />
+                                        <input onChange={handleInputChange} value={product.discount} type="number" name="discount" className="w-35 bg-third rounded bd-1-gray" />
                                     </div>
                                     <div className="w-100 d-flex flex-column mt-1-v color-no-5">
                                         <span className="py-0-5-v">TOTAL PRICE</span>
-                                        <input onChange={handleInputChange} value={product.total} type="number"  name="total" className="w-35 bg-third rounded bd-1-gray" />
+                                        <input onChange={handleInputChange} value={product.total} type="number" name="total" className="w-35 bg-third rounded bd-1-gray" />
                                     </div>
                                     <button className="w-70 mt-2-v bg-second color-white rounded border-0 py-0-5-v font-weight-bold" type="submit">UPDATE</button>
                                 </div>
